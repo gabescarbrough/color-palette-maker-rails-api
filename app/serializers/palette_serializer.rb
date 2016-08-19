16e77c6fbd96e9a9c70a6b4c77622629517f6877
gabescarbrough <gabescarbrough@gmail.com>
2016-08-19 10:55:04 -0400

    Add has_one :user to PaletteSerializer
    Allows access to user info from palette AJAX requests

--- app/serializers/palette_serializer.rb
+++ app/serializers/palette_serializer.rb
@@ -1,3 +1,4 @@
 class PaletteSerializer < ActiveModel::Serializer
-  attributes :id, :palette_name, :color1, :color2, :color3, :color4, :color5, :user_id
+  attributes :id, :palette_name, :color1, :color2, :color3, :color4, :color5
+  has_one :user
 end
