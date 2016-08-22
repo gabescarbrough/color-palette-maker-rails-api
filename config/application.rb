fad836220256805cd1150912abc72fbfa1b89702
gabescarbrough <gabescarbrough@gmail.com>
2016-08-22 13:57:06 -0400

    Change CLIENT_ORIGIN for production
    Change CLIENT_ORIGIN to 'https://gabescarbrough.github.io/'

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module ColorPaletteMaker
     # Cross-Origin Resource Sharing
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_ORIGIN'] || 'http://localhost:8080'
+        origins ENV['CLIENT_ORIGIN'] || 'https://gabescarbrough.github.io/'
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
